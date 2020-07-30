f99eb2298cac0833831e87e5dd9cffb1c4f3ed77
Leandro Torres <leandroftm@gmail.com>
2020-07-30 19:11:27 -0300

    Página de cad video e categoria criados

---
+++ src/pages/Page404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Page404() {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>A página requisitada não existe nesse servidor. Isso é tudo o que sabemos.</p>
+        </div>
+    );
+
+}
+
+export default Page404;
